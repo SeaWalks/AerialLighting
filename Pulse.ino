0c8a583d364c0b66b659981569a6caf847ad54c7
SeaWalks <80789948+SeaWalks@users.noreply.github.com>
2023-11-14 10:11:50 -0800

    Update and rename Aerial.ino to Pulse.ino
    Also added #define parameteres for the background color so it is easier to change.

--- Pulse.ino
+++ Pulse.ino
@@ -2,18 +2,20 @@
 
 #define LED_PIN 6
 #define NUM_LEDS 144
-//This line determines pulse length
+//Define pulse length
 #define PULSE_LENGTH 8
-//These lines determine pusle color
-#define PULSE_COLOR_R 0 
+//Define pulse color (default blue)
+#define PULSE_COLOR_R 0
 #define PULSE_COLOR_G 0
 #define PULSE_COLOR_B 255
-//
+//Define background color (default blue)
+#define BG_COLOR_R 0
+#define BG_COLOR_G 0
+#define BG_COLOR_B 255
+//Define pulse timing
 #define MIN_PULSE_DELAY 50
 #define MAX_PULSE_DELAY 50
 #define NUM_PULSES 5
-#define BACKGROUND_BRIGHTNESS 0  // 5% brightness (0-255 range)
-
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 struct Pulse {
@@ -44,24 +46,19 @@ void loop() {
     if (currentMillis - pulses[i].previousMillis >= pulses[i].delay) {
       // Set the LEDs at the previous position to the background color
       for (int j = pulses[i].position; j < pulses[i].position + PULSE_LENGTH; j++) {
-        strip.setPixelColor(j, strip.Color(0, 0, BACKGROUND_BRIGHTNESS));
+        strip.setPixelColor(j, strip.Color(BG_COLOR_R, BG_COLOR_G, BG_COLOR_B));
       }
-      
       // Move the pulse position
       pulses[i].position++;
-      
       // Check if the pulse reached the end, reset to the beginning
       if (pulses[i].position >= NUM_LEDS) {
         pulses[i].position = 0;
       }
-      
       // Set the LEDs at the new position to the pulse color
       for (int j = pulses[i].position; j < pulses[i].position + PULSE_LENGTH; j++) {
         strip.setPixelColor(j, strip.Color(PULSE_COLOR_R, PULSE_COLOR_G, PULSE_COLOR_B));
       }
-      
       strip.show();
-      
       pulses[i].previousMillis = currentMillis;
       pulses[i].delay = random(MIN_PULSE_DELAY, MAX_PULSE_DELAY);
     }
